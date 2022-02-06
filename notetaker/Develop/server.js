ceb94744b91bf30df42998b88f3acff5e142f0a2
Ben Lu <yudonglv@gmail.com>
2022-02-05 22:48:19 -0800

    a

--- notetaker/Develop/server.js
+++ notetaker/Develop/server.js
@@ -34,19 +34,19 @@ app.get('/notes', (req,res) =>
 //GET route
 app.get('/api/notes', (req,res) => {
       // Send a message to the client
-  res.status(200).json(`${req.method} request received to get reviews`);
+  res.status(200).json(`${req.method} request received to get notes`);
 
   // Log our request to the terminal
-  console.info(`${req.method} request received to get reviews`);
+  console.info(`${req.method} request received to get notes`);
 });
 
 // POST request to add a note
 app.post('/api/notes', (req, res) => {
     // Log that a POST request was received
-    console.info(`${req.method} request received to add a review`);
+    console.info(`${req.method} request received to add a notes`);
   
     // Destructuring assignment for the items in req.body
-    const {title, text} = req.body;
+    const {title, text, id} = req.body;
   
     // If all the required properties are present
     if (title && text) {
@@ -54,10 +54,10 @@ app.post('/api/notes', (req, res) => {
       const newNote = {
         title,
         text,
-        review_id: uuid(),
+        id: uuid(),
       };
   
-      // Obtain existing reviews
+      // Obtain existing notes
       fs.readFile('./db/db.json', 'utf8', (err, data) => {
         if (err) {
           console.error(err);
@@ -65,17 +65,17 @@ app.post('/api/notes', (req, res) => {
           // Convert string into JSON object
           const parsedNotes = JSON.parse(data);
   
-          // Add a new review
+          // Add a new notes
           parsedNotes.push(newNote);
   
-          // Write updated reviews back to the file
+          // Write updated notes back to the file
           fs.writeFile(
             './db/db.json',
             JSON.stringify(parsedNotes, null, 4),
             (writeErr) =>
               writeErr
                 ? console.error(writeErr)
-                : console.info('Successfully updated reviews!')
+                : console.info('Successfully updated notes!')
           );
         }
       });
@@ -88,7 +88,7 @@ app.post('/api/notes', (req, res) => {
       console.log(response);
       res.status(201).json(response);
     } else {
-      res.status(500).json('Error in posting review');
+      res.status(500).json('Error in posting notes');
     }
   });
   
