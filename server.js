d0d18d4abe53d3446fb96305ef8626505bca3575
Ben Lu <yudonglv@gmail.com>
2022-02-05 23:19:36 -0800

    m

--- server.js
+++ server.js
@@ -18,17 +18,17 @@ const { urlencoded } = require('express');
 app.use(express.json());
 app.use(express.urlencoded({ extended: true}));
 
-app.use(express.static('public'));
+app.use(express.static('./notetaker/Develop/public'));
 
 app.get('/', (req, res) => 
 {
-    res.sendFile(path.join(__dirname, '/public/index.html')) 
+    res.sendFile(path.join(__dirname, './notetaker/Develop/public/index.html')) 
 });
 
 //GET request for notes
 app.get('/notes', (req,res) =>
 {
-    res.sendFile(path.join(__dirname, '/public/notes.html'))
+    res.sendFile(path.join(__dirname, './notetaker/Develop/public/notes.html'))
 });
 
 //GET route
@@ -58,7 +58,7 @@ app.post('/api/notes', (req, res) => {
       };
   
       // Obtain existing notes
-      fs.readFile('./db/db.json', 'utf8', (err, data) => {
+      fs.readFile('./notetaker/Develop/db/db.json', 'utf8', (err, data) => {
         if (err) {
           console.error(err);
         } else {
@@ -70,7 +70,7 @@ app.post('/api/notes', (req, res) => {
   
           // Write updated notes back to the file
           fs.writeFile(
-            './db/db.json',
+            './notetaker/Develop/db/db.json',
             JSON.stringify(parsedNotes, null, 4),
             (writeErr) =>
               writeErr
