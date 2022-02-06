533bd26697286bfe6d22e9098a1e52a4dc82abb7
Ben Lu <yudonglv@gmail.com>
2022-02-05 20:44:21 -0800

    try

--- notetaker/Develop/server.js
+++ notetaker/Develop/server.js
@@ -29,7 +29,7 @@ app.get('/notes', (req,res) =>
     res.sendFile(path.join(__dirname, '/public/notes.html'))
 });
 
-//API routes
+//GET route
 app.get('/api/notes', (req,res) => {
       // Send a message to the client
   res.status(200).json(`${req.method} request received to get reviews`);
@@ -39,4 +39,57 @@ app.get('/api/notes', (req,res) => {
 });
 
 // POST request to add a note
-
+app.post('/api/notes', (req, res) => {
+    // Log that a POST request was received
+    console.info(`${req.method} request received to add a review`);
+  
+    // Destructuring assignment for the items in req.body
+    const { title, text} = req.body;
+  
+    // If all the required properties are present
+    if (title && text) {
+      // Variable for the object we will save
+      const newNotes = {
+        title,
+        text,
+        review_id: uuid(),
+      };
+  
+      // Obtain existing reviews
+      fs.readFile('./db/reviews.json', 'utf8', (err, data) => {
+        if (err) {
+          console.error(err);
+        } else {
+          // Convert string into JSON object
+          const parsedNotes = JSON.parse(data);
+  
+          // Add a new review
+          parsedNotes.push(newNotes);
+  
+          // Write updated reviews back to the file
+          fs.writeFile(
+            './db/reviews.json',
+            JSON.stringify(parsedReviews, null, 4),
+            (writeErr) =>
+              writeErr
+                ? console.error(writeErr)
+                : console.info('Successfully updated reviews!')
+          );
+        }
+      });
+  
+      const response = {
+        status: 'success',
+        body: newNotes,
+      };
+  
+      console.log(response);
+      res.status(201).json(response);
+    } else {
+      res.status(500).json('Error in posting review');
+    }
+  });
+  
+  app.listen(PORT, () =>
+    console.log(`App listening at http://localhost:${PORT} 🚀`)
+  );
