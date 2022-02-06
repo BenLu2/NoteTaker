7663f78881c83184bd05f5c5b3b7d477ea4b124c
Ben Lu <yudonglv@gmail.com>
2022-02-05 20:14:15 -0800

    new

--- notetaker/Develop/server.js
+++ notetaker/Develop/server.js
@@ -5,7 +5,18 @@ const fs = require('fs');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
-const path = require('path')
+const path = require('path');
+const { url } = require('inspector');
+const { urlencoded } = require('express');
+
+//middleware
+app.use(express.json());
+app.use(express/urlencoded({ extended: true}));
 
 app.use(express.static('public'));
+
+app.get('/', (req, res) => 
+{
+    res.sendFile(path.join(__dirname, '/public/index.html')) 
+});
 
