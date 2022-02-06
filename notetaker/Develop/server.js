d274dbef15e7c98e4cccb9dd1e02adce8e71229d
Ben Lu <yudonglv@gmail.com>
2022-02-05 20:41:52 -0800

    more

--- notetaker/Develop/server.js
+++ notetaker/Develop/server.js
@@ -1,7 +1,10 @@
 const express = require('express');
 const path = require('path')
+const Notes = require('./db/db.json')
 const fs = require('fs');
 
+// Helper method for generating unique ids
+const uuid = require('./helper/uuid');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -19,4 +22,21 @@ app.get('/', (req, res) =>
 {
     res.sendFile(path.join(__dirname, '/public/index.html')) 
 });
+
+//GET request for notes
+app.get('/notes', (req,res) =>
+{
+    res.sendFile(path.join(__dirname, '/public/notes.html'))
+});
+
+//API routes
+app.get('/api/notes', (req,res) => {
+      // Send a message to the client
+  res.status(200).json(`${req.method} request received to get reviews`);
+
+  // Log our request to the terminal
+  console.info(`${req.method} request received to get reviews`);
+});
+
+// POST request to add a note
 
